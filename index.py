af03aff17d6d79cc196c90ee8ffc38ea9d0da915
coolwhipki <kirajanegirl@gmail.com>
2023-03-02 18:31:24 -0700

    added self to burger count

--- index.py
+++ index.py
@@ -1,16 +1,15 @@
-#Group Members
 #Cassidy Hardisty, Kira Coleman, Tiffany Zang, Ryan Hawkins, Grace Chou, Ella Cook
 
 # Create an Order class
 #import random
 import random
 class Order() :
-    def __init__ (self, burger_count) :
+    def __init__ (self) :
         self.burger_count = Order.randomBurgers()
 
     def randomBurgers(self) :
-        burger_count = random.randint(1,20)
-        return burger_count
+        self.burger_count = random.randint(1,20)
+        return self.burger_count
 # Create a constructor that defines an instance variable called burger_count
 # Create a method called randomBurgers that returns a number between 1 and 20
 # The constructor should call the randomBurgers() method and assign the return value to the burger_count instance variable
