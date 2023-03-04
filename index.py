e4e0782d2ed9cabe33d94f30a1cabfa092b7d55a
Cassidy Hardisty <cassidy.hardisty@gmail.com>
2023-03-03 18:28:30 -0700

    Fixed code for dictionary so it runs

--- index.py
+++ index.py
@@ -18,12 +18,13 @@ class Order() :
 class Person() :
     def __init__(self) :
         # Create a constructor that defines an instance variable called customer_name 
-        # The Person constructor should call the randomName() method and assign the return value (a random name) to the customer_name instance variable
+        # The Person constructor should call the randomName() method and assign the 
+        # return value (a random name) to the customer_name instance variable
         self.customer_name = self.randomName() 
 
     # Create a method called randomName() that contains a list of 9 names:
-    # This method randomly returns one of the 9 names when called
-    # asCustomers = ["Jefe", "El Guapo", "Lucky Day", "Ned Nederlander", "Dusty Bottoms", "Harry Flugleman", "Carmen", "Invisible Swordsman", "Singing Bush"]
+    # This method randomly returns one of the 9 names when called: ["Jefe", "El Guapo", "Lucky Day", "Ned Nederlander", 
+    # "Dusty Bottoms", "Harry Flugleman", "Carmen", "Invisible Swordsman", "Singing Bush"]
     def randomName(self) :
         self.asCustomers = ["Jefe", "El Guapo", "Lucky Day", "Ned Nederlander", "Dusty Bottoms", "Harry Flugleman", "Carmen", "Invisible Swordsman", "Singing Bush"]
         return random.choice(self.asCustomers)
@@ -40,23 +41,30 @@ class Customer(Person) :
 # This variable will represent your line of customers (objects) waiting outside to place their hamburger orders
 queueCustomers = []
 
-
+# Create a variable for a Dictionary with keys of type string and values of type int.
 # This variable will hold information about each customer 
+dCustomer = {}
 
 # Put 100 customers into the queue. Each customer object will already have a random number of burgers for each order
 for iCount in range(0, 101):
     queueCustomers.append(Customer())
 
-# Create a variable for a Dictionary with keys of type string and values of type int.
-dCustomer = {} 
-for iCount in range(0, len(queueCustomers)) :
-    dCustomer[queueCustomers[iCount].customer_name] = queueCustomers[iCount].order.burger_count
+#loop runs while queue has items in it
+while (len(queueCustomers) > 0) :
+    # Make sure there is a key in the dictionary for each customer before you try incrementing their total! 
+    # Otherwise, add the customer to the dictionary.
+    if (dCustomer.get(queueCustomers[0].customer_name)) :
+        #increment total for that customer name
+        dCustomer[queueCustomers[0].customer_name] += queueCustomers[0].order.burger_count
+    else:
+        #adding customer to the dictionary because it doesn't exist already
+        dCustomer[queueCustomers[0].customer_name] = queueCustomers[0].order.burger_count
+    
+    queueCustomers.pop(0)
 
-# Make sure there is a key in the dictionary for each customer before you try incrementing their total! 
-# Otherwise, add the customer to the dictionary.
-
-# Print out each customer and their total burgers ordered sorted by the most number of burgers ordered
+# sort the dictionary from lowest to highest
 sortedCustomers = sorted(dCustomer.items(), key=lambda x: x[1], reverse=True)
 
-for iCount in range(0, len(queueCustomers)):
-    # print(sortedCustomers[iCount].ljust(19))
+# Print out each customer and their total burgers ordered sorted by the most number of burgers ordered
+for iCount in range(0, len(sortedCustomers)):
+    print(sortedCustomers[iCount][0].ljust(19), sortedCustomers[iCount][1])
